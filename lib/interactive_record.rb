8ced71a1762278d6530f6eaa5e7249d8812fe763
Emily Morgan <emorgan05@gmail.com>
2017-10-25 19:02:25 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
   end
 
   def self.find_by(hash)
-    property = hash.keys
+    property = hash.keys.to_s
     value = hash[:property]
     sql = "SELECT * FROM #{self.table_name} WHERE #{property} = '#{value}'"
 
